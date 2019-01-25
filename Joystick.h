fc21f82490e76688ed1fbddd17f38f5b24406ed4
Michael Kubina <Kubina.Michael@googlemail.com>
2019-01-25 22:12:11 +0100

    updated license header in .cpp and .h

--- Joystick.h
+++ Joystick.h
@@ -1,7 +1,19 @@
 ﻿/*
-  joystick.h - Library for using a analog 2-axis joystick with select-button.
-  Created by Michael Kubina, January 25, 2019.
-  Released into the public domain.
+	joystick.h - Library for using a analog 2-axis joystick with select-button.
+	Copyright (C) by Michael Kubina, January 25, 2019.
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
 #ifndef Joystick_h
